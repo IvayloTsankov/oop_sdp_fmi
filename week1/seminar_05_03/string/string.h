1f6049eaeae8c49f08d66012cb58d32ca956c852
IvayloTsankov <zub40000@gmail.com>
2014-03-11 23:20:38 +0200

    declare string interface

--- week1/seminar_05_03/string/string.h
+++ week1/seminar_05_03/string/string.h
@@ -1,3 +1,37 @@
+#ifndef STRING_H_
+#define STRING_H_
 
-jg:wq
+class String
+{
+public:
+    String();
+    String(const char* val);
+    ~String();
+    
+    //assignment and copy
+    String& operator = (const String& str);
+    String(const String& rstr);
 
+public:
+    bool empty() const;
+    void clean();
+    const char* c_str() const;
+
+    void append(String& str);
+    void append(char* str);
+
+    const char* find(char* val); //return pointer to first occurence
+   
+    String operator + (const String& str);
+    String& operator += (const String& str);
+    char& operator [] (int); //unsigned is better
+    const char& operator p[] (int);
+    
+    String substr(unsigned pos, unsigned size);
+
+private:
+    char* data_;
+    unsigned size_;
+};
+
+#endif // STRING_H_
